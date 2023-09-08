a78bcddd9ede8ea20841bca13186156c26b83628
Praise Balogun <pbalogun@axxess.com>
2023-09-08 17:41:42 +0100

    added date validation for student

--- student_teacher_frontend/src/components/student-form/StudentForm.jsx
+++ student_teacher_frontend/src/components/student-form/StudentForm.jsx
@@ -39,72 +39,77 @@ const StudentForm = ({isopend}) => {
 
 
   return (
-    <div className={`teacher-form ${toggle}`}>
-      <form>
-        <div className="form first">
-          <div className="details personal">
-            <span className="title">Student Details</span>
+		<div className={`teacher-form ${toggle}`}>
+			<form>
+				<div className='form first'>
+					<div className='details personal'>
+						<span className='title'>Student Details</span>
 
-            <div className="fields">
-              <div className="input-fields">
-                <label style={{color: "black"}}>Name</label>
-                <input 
-                  type='text' 
-                  placeholder='Enter given name' 
-                  onChange={(e)=> setName(e.target.value)}
-                  required/>
-              </div>
-              <div className="input-fields">
-                <label style={{color: "black"}}>Surname</label>
-                <input 
-                  type='text' 
-                  placeholder='Enter surname here' 
-                  onChange={(e)=> setSurname(e.target.value)}
-                  required/>
-              </div>
-              <div className="input-fields">
-                <label style={{color: "black"}}>National ID</label>
-                <input 
-                  type='text' 
-                  placeholder='Enter national identity number' 
-                  onChange={(e) => setNationalIdNumber(e.target.value)}
-                  required/>
-              </div>
+						<div className='fields'>
+							<div className='input-fields'>
+								<label style={{ color: 'black' }}>Name</label>
+								<input
+									type='text'
+									placeholder='Enter given name'
+									onChange={(e) => setName(e.target.value)}
+									required
+								/>
+							</div>
+							<div className='input-fields'>
+								<label style={{ color: 'black' }}>Surname</label>
+								<input
+									type='text'
+									placeholder='Enter surname here'
+									onChange={(e) => setSurname(e.target.value)}
+									required
+								/>
+							</div>
+							<div className='input-fields'>
+								<label style={{ color: 'black' }}>National ID</label>
+								<input
+									type='text'
+									placeholder='Enter national identity number'
+									onChange={(e) => setNationalIdNumber(e.target.value)}
+									required
+								/>
+							</div>
 
-              <div className="input-fields">
-                <label style={{color: "black"}}>Student Number</label>
-                <input 
-                  type='text' 
-                  placeholder='Enter national identity number' 
-                  onChange={(e) => setStudentNumber(e.target.value)}
-                  required/>
-              </div>
+							<div className='input-fields'>
+								<label style={{ color: 'black' }}>Student Number</label>
+								<input
+									type='text'
+									placeholder='Enter national identity number'
+									onChange={(e) => setStudentNumber(e.target.value)}
+									required
+								/>
+							</div>
 
+							<div className='input-fields'>
+								<label style={{ color: 'black' }}>Gender</label>
+								<select className='select-input'>
+									<option>Male</option>
+									<option>Femele</option>
+									<option>Prefer Not Say</option>
+								</select>
+							</div>
 
-              <div className="input-fields">
-                <label style={{color: "black"}}>Gender</label>
-                <select className='select-input'>
-                  <option>Male</option>
-                  <option>Femele</option>
-                  <option>Prefer Not Say</option>
-                </select> 
-              </div>
-
-              <div className="input-fields">
-                <label style={{color: "black"}}>Date of Birth</label>
-                <input 
-                  type='date' 
-                  placeholder='Date product was recieved' 
-                  onChange={(e) => setDobstring(e.target.value)}
-                  required/>
-              </div>
-            </div>
-            <SubmitButton buttonAction={handleSubmitStudent}/>
-          </div>
-        </div>
-      </form>
-    </div>
-  )
+							<div className='input-fields'>
+								<label style={{ color: 'black' }}>Date of Birth</label>
+								<input
+									type='date'
+									min='2001-01-01'
+									placeholder='Date product was recieved'
+									onChange={(e) => setDobstring(e.target.value)}
+									required
+								/>
+							</div>
+						</div>
+						<SubmitButton buttonAction={handleSubmitStudent} />
+					</div>
+				</div>
+			</form>
+		</div>
+	)
 }
 
 export default StudentForm
